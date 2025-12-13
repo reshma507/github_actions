74c19cb07b00acc47b4c292d4b3b31b53bb6e09e
“reshma” <shaikreshma1508@gmail.com>
2025-12-14 01:49:47 +0530

    adding CI/CD github actions

--- config/database.ts
+++ config/database.ts
@@ -12,27 +12,27 @@
 //     debug: false,
 //   },
 // });
-// export default ({ env }) => ({
-//   connection: {
-//     client: 'sqlite',
-//     connection: {
-//       filename: env('DATABASE_FILENAME', '/data/data.db'),
-//     },
-//     useNullAsDefault: true,
-//   },
-// });
-
 export default ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: 'sqlite',
     connection: {
-      host: env('DATABASE_HOST', 'strapi-postgres'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi_db'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi123'),
-      ssl: false,
+      filename: env('DATABASE_FILENAME', '/data/data.db'),
     },
-    acquireConnectionTimeout: 60000,
+    useNullAsDefault: true,
   },
 });
+
+// export default ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST', 'strapi-postgres'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME', 'strapi_db'),
+//       user: env('DATABASE_USERNAME', 'strapi'),
+//       password: env('DATABASE_PASSWORD', 'strapi123'),
+//       ssl: false,
+//     },
+//     acquireConnectionTimeout: 60000,
+//   },
+// });
