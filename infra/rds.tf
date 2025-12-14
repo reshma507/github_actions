008df01be6409594f5d44ae9a700b3a52c4ac97a
“reshma” <shaikreshma1508@gmail.com>
2025-12-14 11:29:19 +0530

    adding CI/CD github actions

--- infra/rds.tf
+++ infra/rds.tf
@@ -16,6 +16,6 @@ resource "aws_db_instance" "strapi" {
   publicly_accessible    = false
   skip_final_snapshot    = true
 
-  vpc_security_group_ids = [aws_security_group.rds_sg-reshma.id]
+  vpc_security_group_ids = [aws_security_group.rds_sg_reshma.id]
   db_subnet_group_name   = aws_db_subnet_group.strapi.name
 }
